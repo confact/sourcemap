af8891af329811ee5d141cdc085ddd54f4a2296d
Håkan <hakan@dun.se>
2023-08-28 19:46:03 +0200

    optimizing and added missing methods

--- src/sourcemap_parser/source_map.cr
+++ src/sourcemap_parser/source_map.cr
@@ -22,7 +22,6 @@ module SourceMap
     property sourcemap_uri : URI = URI.parse("")
 
     def initialize(@version, @sources, @names, @file, @mappings)
-      parse_mappings(mappings || "")
     end
 
     def after_initialize
@@ -38,25 +37,13 @@ module SourceMap
       from_json(string)
     end
 
-    # All the numbers in SourceMaps are stored as differences from each other,
-    # so we need to remove the difference every time we read a number.
     def undiff(int, type)
-      if previous[type]?
-        previous[type] += int
-      else
-        previous[type] = int
-      end
-      previous[type]
+      previous[type] = (previous[type]? || 0) + int
     end
 
-    # Parse the mapping string from a SourceMap.
-    #
-    # The mappings string contains one comma-separated list of segments per line
-    # in the output file, these lists are joined by semi-colons.
-    #
     def parse_mappings(string) : Array(Mapping)
-      previous = {} of String => Int32
-      @mappings.split(";").each_with_index do |line, line_idx|
+      previous.clear
+      string.split(";").each_with_index do |line, line_idx|
         previous["generated_col"] = 0
         line.split(",").each do |segment|
           next if segment.empty?
@@ -66,11 +53,6 @@ module SourceMap
       parsed_mappings.sort_by! { |x| [x.generated_line, x.generated_column] }
     end
 
-    # Parse an individual mapping.
-    #
-    # This is a list of variable-length-quanitity, with 1, 4 or 5 items. See the spec
-    # https://docs.google.com/document/d/1U1RGAehQwRypUTovF1KRlpiOFze0b-_2gc6fAH0KY0k/edit
-    # for more details.
     def parse_mapping(segment, line_num) : Mapping
       item = VLQ.decode(segment)
 
@@ -78,50 +60,62 @@ module SourceMap
         raise Exception.new("In map for #{file}:#{line_num}: unparseable item: #{segment}")
       end
 
-      map = if item.size == 4
-              Mapping.new(
-                line_num,
-                undiff(item[0], "generated_column"),
-                sources[undiff(item[1], "source_id")],
-                undiff(item[2], "source_line") + 1,
-                undiff(item[3], "source_column")
-              )
-            elsif item.size == 5
-              Mapping.new(
-                line_num,
-                undiff(item[0], "generated_column"),
-                sources[undiff(item[1], "source_id")],
-                undiff(item[2], "source_line") + 1,
-                undiff(item[3], "source_column"),
-                names[undiff(item[4], "name_id")]
-              )
-            else
-              Mapping.new(line_num, undiff(item[0], "generated_column"))
-            end
+      case item.size
+      when 4
+        source_idx = undiff(item[1], "source_id")
+        map = Mapping.new(
+          line_num,
+          undiff(item[0], "generated_column"),
+          sources[source_idx],
+          undiff(item[2], "source_line") + 1,
+          undiff(item[3], "source_column")
+        )
+      when 5
+        source_idx = undiff(item[1], "source_id")
+        name_idx = undiff(item[4], "name_id")
+        map = Mapping.new(
+          line_num,
+          undiff(item[0], "generated_column"),
+          sources[source_idx],
+          undiff(item[2], "source_line") + 1,
+          undiff(item[3], "source_column"),
+          names[name_idx]
+        )
+      else
+        map = Mapping.new(line_num, undiff(item[0], "generated_column"))
+      end
 
       if map.source_path
          map.source_content = source_content_for(map.source_path.not_nil!)
       end
 
-      if map.generated_column < 0
-        raise Exception.new("In map for #{file}:#{line_num}: unexpected generated_column: #{map.generated_column}")
-      elsif map.source_line < 1
-        raise Exception.new("In map for #{file}:#{line_num}: unexpected source_line: #{map.source_line}")
-      elsif map.source_column < 0
-        raise Exception.new("In map for #{file}:#{line_num}: unexpected source_column: #{map.source_column}")
-      end
+      raise Exception.new("In map for #{file}:#{line_num}: unexpected generated_column: #{map.generated_column}") if map.generated_column < 0
+      raise Exception.new("In map for #{file}:#{line_num}: unexpected source_line: #{map.source_line}") if map.source_line < 1
+      raise Exception.new("In map for #{file}:#{line_num}: unexpected source_column: #{map.source_column}") if map.source_column < 0
 
       map
     end
 
-    def source_content_for(source_id : String) : String?
-      return nil if sources_content.empty?
-      return nil if source_index_for(source_id).nil?
-      sources_content[source_index_for(source_id).not_nil!]?
+    # Retrieve a Mapping for a specific line and column in the generated code
+    def mapping_for(generated_line : Int32, generated_column : Int32) : Mapping?
+      parsed_mappings.find do |mapping|
+        mapping.generated_line == generated_line && mapping.generated_column == generated_column
+      end
     end
 
-    def source_index_for(source_id : String) : Int32?
+    # Retrieve all mappings for a specific source path
+    def mappings_for_source(source_path : String) : Array(Mapping)
+      parsed_mappings.select { |mapping| mapping.source_path == source_path }
+    end
+
+    private def source_content_for(source_id : String) : String?
+      idx = source_index_for(source_id)
+      idx ? sources_content[idx] : nil
+    end
+
+    private def source_index_for(source_id : String) : Int32?
       sources.index(source_id)
     end
   end
 end
+
